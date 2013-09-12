0fe237381ef9b57d18e1d58b31e6914e28135cab
Robert Metzger <metzgerr@web.de>
2013-09-12 13:58:04 +0200

    removed travis script

--- .travis_build.sh
+++
@@ -1,8 +0,0 @@
-mvn verify # assuming that hadoop_v1 profile is activated by default
-if [ $? -ne 0 ]; then
-	echo "mvn verify failed for hadoop_v1 maven profile. Aborting build!"
-	return $?
-fi
-echo "mvn verify for hadoop_v1 was successful. Verifying hadoop yarn"
-mvn -Phadoop_yarn clean verify
-return $?
