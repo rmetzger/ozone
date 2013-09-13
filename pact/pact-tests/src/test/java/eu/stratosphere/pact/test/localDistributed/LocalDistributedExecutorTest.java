f2bb3f4c65d3b1b75318a42e3341947b5d95645b
Robert Metzger <metzgerr@web.de>
2013-09-13 14:12:41 +0200

    Minor fix

--- pact/pact-tests/src/test/java/eu/stratosphere/pact/test/localDistributed/LocalDistributedExecutorTest.java
+++ pact/pact-tests/src/test/java/eu/stratosphere/pact/test/localDistributed/LocalDistributedExecutorTest.java
@@ -21,7 +21,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import eu.stratosphere.nephele.util.Logging;
-import eu.stratosphere.pact.client.LocalExecutorTest;
 import eu.stratosphere.pact.example.wordcount.WordCount;
 
 
