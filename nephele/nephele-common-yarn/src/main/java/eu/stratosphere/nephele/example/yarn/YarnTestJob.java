7c13d611a76970ca4a7f76df4d387eb42319e203
Robert Metzger <metzgerr@web.de>
2013-10-05 18:21:05 +0200

    added comment

--- nephele/nephele-common-yarn/src/main/java/eu/stratosphere/nephele/example/yarn/YarnTestJob.java
+++ nephele/nephele-common-yarn/src/main/java/eu/stratosphere/nephele/example/yarn/YarnTestJob.java
@@ -70,7 +70,7 @@ public final class YarnTestJob {
 
 		// Set configuration
 		final Configuration conf = new Configuration();
-		conf.setInteger(YarnJobClient.YARN_APPLICATIONMASTER_MEMORY_KEY, 256);
+		conf.setInteger(YarnJobClient.YARN_APPLICATIONMASTER_MEMORY_KEY, 256); // main memory for application master
 		conf.setString(YarnJobClient.NEPHELE_HOME_KEY, "/home/tobias/stratosphere-0.4-ozone-SNAPSHOT");
 		conf.setString("jobmanager.rpc.address", "localhost");
 
