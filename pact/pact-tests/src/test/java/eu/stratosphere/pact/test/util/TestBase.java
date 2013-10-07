3dbc4011bb810e545173c3aec93dab0e694d593c
Robert Metzger <metzgerr@web.de>
2013-10-07 18:14:39 +0200

    java 6 compat

--- pact/pact-tests/src/test/java/eu/stratosphere/pact/test/util/TestBase.java
+++ pact/pact-tests/src/test/java/eu/stratosphere/pact/test/util/TestBase.java
@@ -22,7 +22,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.spi.FileSystemProvider;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
