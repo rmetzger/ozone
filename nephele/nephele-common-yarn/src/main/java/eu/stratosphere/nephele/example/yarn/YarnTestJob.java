dc6f2840eb6f760e083710eddded4a7b5a69aedf
Robert Metzger <metzgerr@web.de>
2013-10-25 10:55:57 +0200

    add missing dependency for yarn

--- nephele/nephele-common-yarn/src/main/java/eu/stratosphere/nephele/example/yarn/YarnTestJob.java
+++ nephele/nephele-common-yarn/src/main/java/eu/stratosphere/nephele/example/yarn/YarnTestJob.java
@@ -71,7 +71,7 @@ public final class YarnTestJob {
 		// Set configuration
 		final Configuration conf = new Configuration();
 		conf.setInteger(YarnJobClient.YARN_APPLICATIONMASTER_MEMORY_KEY, 256); // main memory for application master
-		conf.setString(YarnJobClient.NEPHELE_HOME_KEY, "/home/tobias/stratosphere-0.4-ozone-SNAPSHOT");
+		conf.setString(YarnJobClient.NEPHELE_HOME_KEY, "/home/robert/Projekte/ozone/ozone/");
 		conf.setString("jobmanager.rpc.address", "localhost");
 
 		// Create job client and launch job
