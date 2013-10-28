babc0027bed1ae80ae2fa9e03af866980fcc9536
Robert Metzger <metzgerr@web.de>
2013-10-28 23:50:47 +0100

    finalized non-yarn package

--- nephele/nephele-noyarn/src/main/java/eu/stratosphere/nephele/yarn/client/YarnJobClient.java
+++ nephele/nephele-noyarn/src/main/java/eu/stratosphere/nephele/yarn/client/YarnJobClient.java
@@ -1,1 +1,49 @@
-do
+package eu.stratosphere.nephele.yarn.client;
+
+import java.io.IOException;
+
+import eu.stratosphere.nephele.client.JobCancelResult;
+import eu.stratosphere.nephele.client.JobClient;
+import eu.stratosphere.nephele.client.JobExecutionException;
+import eu.stratosphere.nephele.client.JobProgressResult;
+import eu.stratosphere.nephele.client.JobSubmissionResult;
+
+
+/**
+ * This class is included if Stratosphere is not build
+ * with Yarn Support.
+ * @See {@link NonYarnPackageException}
+ */
+public class YarnJobClient implements JobClient {
+
+	@Override
+	public int getRecommendedPollingInterval() throws IOException {
+		throw new NonYarnPackageException();
+	}
+
+	@Override
+	public JobProgressResult getJobProgress() throws IOException {
+		throw new NonYarnPackageException();
+	}
+
+	@Override
+	public JobSubmissionResult submitJob() throws IOException {
+		throw new NonYarnPackageException();
+	}
+
+	@Override
+	public long submitJobAndWait() throws IOException, JobExecutionException {
+		throw new NonYarnPackageException();
+	}
+
+	@Override
+	public JobCancelResult cancelJob() throws IOException {
+		throw new NonYarnPackageException();
+	}
+
+	@Override
+	public void close() {
+		throw new NonYarnPackageException();
+	}
+	
+}
