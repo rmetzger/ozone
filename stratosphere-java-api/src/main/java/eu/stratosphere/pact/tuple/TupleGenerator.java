73fe89d61f8d96135a3579bf5e32b89f0fbd46b8
StephanEwen <stephan.ewen@tu-berlin.de>
2013-12-18 12:19:18 +0100

    Added and unified Apache License headers in all files.

--- stratosphere-java-api/src/main/java/eu/stratosphere/pact/tuple/TupleGenerator.java
+++ stratosphere-java-api/src/main/java/eu/stratosphere/pact/tuple/TupleGenerator.java
@@ -1,6 +1,6 @@
 /***********************************************************************************************************************
  *
- * Copyright (C) 2012 by the Stratosphere project (http://stratosphere.eu)
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -138,7 +138,7 @@ public class TupleGenerator {
 	private static String HEADER = 
 		"/***********************************************************************************************************************\n" +
 		" *\n" +
-		" * Copyright (C) 2010 - 2013 by the Stratosphere project (http://stratosphere.eu)\n" +
+		" * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)\n" +
 		" *\n" +
 		" * Licensed under the Apache License, Version 2.0 (the \"License\"); you may not use this file except in compliance with\n" +
 		" * the License. You may obtain a copy of the License at\n" +
